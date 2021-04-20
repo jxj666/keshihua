cca862d8505285775baca8da8e503926bd88e445
jinxiaojian :-) <228322991@qq.com>
2021-04-21 00:03:42 +0800

    fix

--- 18 如何生成简单动画/4/Animate.js
+++ 18 如何生成简单动画/4/Animate.js
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2021-04-20 00:28:41
+ * @LastEditTime: 2021-04-21 00:01:35
  * @LastEditors: jinxiaojian
  */
 
@@ -11,17 +11,18 @@ export class Animator {
   }
 
   animate(target, update) {
-    let frameIndex = 0;
+    // let frameIndex = 0;
     const timing = new Timing(this.timing);
     // console.log(timing)
     return new Promise((resolve) => {
       function next() {
-        if(update({target, frameIndex, timing}) !== false && !timing.isFinished) {
+        // if(update({target, frameIndex, timing}) !== false && !timing.isFinished) {
+        if(update({target, timing}) !== false && !timing.isFinished) {
           requestAnimationFrame(next);
         } else {
           resolve(timing);
         }
-        frameIndex++;
+        // frameIndex++;
       }
       next();
     });
