dcaeba80da6e66065db4a77953bd23b62b0f349e
JinXiaoJian <jinxiaojian@threatbook.cn>
2021-04-25 11:11:02 +0800

    fix

--- WebGL/7/1.js
+++ WebGL/7/1.js
@@ -378,7 +378,11 @@ function drawScene (gl, programInfo, buffers, texture, deltaTime) {
     [-0.0, 0.0, -6.0]);  // amount to translate
   mat4.rotate(modelViewMatrix,  // destination matrix
     modelViewMatrix,  // matrix to rotate
-    cubeRotation,     // amount to rotate in radians
+    cubeRotation * .7,     // amount to rotate in radians
+    [1, 0, 0]);       // axis to rotate around (Z)
+  mat4.rotate(modelViewMatrix,  // destination matrix
+    modelViewMatrix,  // matrix to rotate
+    cubeRotation * .7,     // amount to rotate in radians
     [0, 0, 1]);       // axis to rotate around (Z)
   mat4.rotate(modelViewMatrix,  // destination matrix
     modelViewMatrix,  // matrix to rotate
