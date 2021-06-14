a6635bd1803992679cc65e149fab65e4a8e0a863
jinxiaojian <228322991@qq.com>
2021-06-14 18:11:10 +0800

    fix

--- anli/0613曲条/my/main.js
+++ anli/0613曲条/my/main.js
@@ -1,4 +1,10 @@
 /*
- * @LastEditTime: 2021-06-14 00:24:14
+ * @LastEditTime: 2021-06-14 18:10:57
  * @LastEditors: jinxiaojian
  */
+const chartData = []
+const numOfBars = chartData.length;
+const start = { left: 100, right: 350, padding: 5 }
+const bar = { width: 20, padding: 10 }
+const triangle = { width: bar.width / 2, height: bar.width, padding: 2, num: 3 }
+const minRadius = 100
