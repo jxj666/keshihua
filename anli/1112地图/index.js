7530d5e0dda2ff5e78567feeb121d6231493afa4
jinxiaojian <228322991@qq.com>
2021-11-15 02:02:50 +0800

    fix

--- anli/1112地图/index.js
+++
@@ -1,1 +0,0 @@
-export {default} from "./b03383286d6ad9f0@852.js";
--- anli/1121地图/main.js
+++ anli/1121地图/main.js
@@ -1,4 +1,47 @@
 /*
- * @LastEditTime: 2021-11-15 00:49:50
+ * @LastEditTime: 2021-11-15 01:54:40
  * @LastEditors: jinxiaojian
  */
+var svg = d3.select("svg"),
+  width = +svg.attr("width"),
+  height = +svg.attr("height");
+
+var projection = d3.geoMercator()
+  .scale((width - 3) / (2 * Math.PI))
+  .translate([width / 2, height / 2]);
+
+var path = d3.geoPath()
+  .projection(projection);
+
+var graticule = d3.geoGraticule();
+
+svg.append("defs").append("path")
+  .datum({ type: "Sphere" })
+  .attr("id", "sphere")
+  .attr("d", path);
+
+svg.append("use")
+  .attr("class", "stroke")
+  .attr("xlink:href", "#sphere");
+
+svg.append("use")
+  .attr("class", "fill")
+  .attr("xlink:href", "#sphere");
+
+svg.append("path")
+  .datum(graticule)
+  .attr("class", "graticule")
+  .attr("d", path);
+
+d3.json("./map.json", function (error, world) {
+  if (error) throw error;
+  var groups = svg.append('g');
+  groups.selectAll("path")
+    .data(world.features)
+    .enter()
+    .append("path")
+    .attr("class", 'country')
+    .style('fill', 'gray')
+    .attr("d", path)
+
+});
