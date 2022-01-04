66c4afe77cecda29d6d5cae781d8da768725b98e
jinxiaojian <228322991@qq.com>
2022-01-04 14:22:03 +0800

    fix

--- anli/0104地图/main.js
+++ anli/0104地图/main.js
@@ -1,15 +1,18 @@
 /*
- * @LastEditTime: 2022-01-04 11:51:44
+ * @LastEditTime: 2022-01-04 14:21:32
  * @LastEditors: jinxiaojian
  */
-let mockData = {
+let warnData = {
   "CHN": 'rgb(219, 74, 73)',
   "USA": 'rgb(225, 146, 136)',
   "IND": 'rgb(245, 195, 188)',
+  "DEU": 'rgb(245, 195, 188)',
+  "FRA": 'rgb(245, 195, 188)',
 }
 var svg = d3.select("svg"),
   width = +svg.attr("width")
 svg.attr('style', 'background:rgb(43, 34, 35)')
+var tootip = d3.select('body').append('div').attr('class', 'tooltip').style('opacity', 0.0)
 
 d3.json("./map2.json", function (error, cData) {
   if (error) throw error
@@ -36,15 +39,15 @@ function mainPic ({
       .append("path")
       .attr("class", 'country')
       .style('fill', (country) => {
-        if (mockData[country.id]) {
+        if (warnData[country.id]) {
           if (cData[country.properties.name]) {
-            // console.log(path.context(cData[country.properties.name]))
-            console.log(path)
-            info(cData[country.properties.name], country, groups,path)
+            // 首先如果有记载位置先获取,否则计算中心点
+            info({ xy: projection(cData[country.properties.name]), country, groups, warnData: warnData[country.id] })
+          } else {
+            info({ xy: path.centroid(country), country, groups, warnData: warnData[country.id] })
           }
-          // info(path.centroid(country), country, groups)
         }
-        return mockData[country.id] || '#fff'
+        return warnData[country.id] || '#fff'
       })
       .style('stroke', '#eee')
       .attr("d", path)
@@ -52,15 +55,29 @@ function mainPic ({
 }
 
 //添加部件
-function info (xy, country, groups,path) {
+function info ({ xy, country, groups, warnData }) {
   let xyData = xy
   let gTag = groups.append("g").attr('class', 'tag')
-  gTag.append("circle")
+  let circle = gTag.append("circle")
     .attr('cx', xyData[0])
     .attr('cy', xyData[1])
-    .attr('r', 3)
-    .attr('fill', '#333')
-    .attr('d', path)
+    .attr('r', 5)
+    .attr('fill', '#fff')
+    .attr('stroke', '#999')
+    .attr('style', 'cursor:pointer')
+    .on('mouseover', d => {
+      console.log(circle)
+      circle.attr('stroke', 'red')
+      tootip.html(`<div>
+<div>${country.properties.name}</div>
+<div>攻击者数量: <span style="color: ${warnData};">???</span></div>
+<div>攻击占比: <span style="color: ${warnData};">???</span></div>
+      </div>`).style('left', d3.event.pageX + 20 + 'px').style('top', d3.event.pageY + 20 + 'px').style('opacity', 1.0).style('z-index', '1000')
+    })
+    .on("mouseout", d => {
+      circle.attr('stroke', '#999')
+      tootip.style('opacity', 0).style('z-index', '-1000')
+    })
 
   // gTag.append("circle")
   //   .attr('cx', xyData[0])
